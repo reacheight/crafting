7f119752d3b5a61d4cbd142bef6a4b820ebeec8f
Roman Mendaliev <reacheight8@gmail.com>
2026-06-08 00:57:18 +0300

    feat(interpreter): add basic expr evaluation

---
+++ src/Lox/Interpreting/Interpreter.cs
@@ -0,0 +1,63 @@
+﻿using Lox.Parsing;
+
+namespace Lox.Interpreting;
+
+public class Interpreter
+{
+    public LoxValue EvaluateExpr(Expr expr) => expr switch
+    {
+        Literal literal => literal.Value,
+        Grouping grouping => EvaluateExpr(grouping.Expr),
+        UnaryExpr unary => EvaluateUnary(unary),
+        BinaryExpr binary => EvaluateBinary(binary),
+        Ternary ternary => EvaluateTernary(ternary),
+    };
+
+    private LoxValue EvaluateUnary(UnaryExpr unary)
+    {
+        var operandValue = EvaluateExpr(unary.Expr);
+        return unary.Operator switch
+        {
+            Not => !EvaluateAsBool(operandValue),
+            Negate => -(double)operandValue.Value,
+        };
+    }
+
+    private LoxValue EvaluateBinary(BinaryExpr binary)
+    {
+        var leftValue = EvaluateExpr(binary.Left);
+        var rightValue = EvaluateExpr(binary.Right);
+
+        return binary.Operator switch
+        {
+            Add => (leftValue, rightValue) switch
+            {
+                (double leftNumber, double rightNumber) => leftNumber + rightNumber,
+                (string leftString, string rightString) => leftString + rightString,
+            },
+            Substract => (double)leftValue.Value - (double)rightValue.Value,
+            Divide => (double)leftValue.Value / (double)rightValue.Value,
+            Multiply => (double)leftValue.Value * (double)rightValue.Value,
+
+            Greater => (double)leftValue.Value > (double)rightValue.Value,
+            GreaterEqual => (double)leftValue.Value >= (double)rightValue.Value,
+            Less => (double)leftValue.Value < (double)rightValue.Value,
+            LessEqual => (double)leftValue.Value <= (double)rightValue.Value,
+
+            Equal => leftValue.Value == rightValue.Value,
+            NotEqual => leftValue.Value != rightValue.Value,
+        };
+    }
+
+    private LoxValue EvaluateTernary(Ternary ternary)
+        => EvaluateAsBool(EvaluateExpr(ternary.Condition))
+            ? EvaluateExpr(ternary.OnTrue)
+            : EvaluateExpr(ternary.OnFalse);
+
+    private static bool EvaluateAsBool(LoxValue literal) => literal switch
+    {
+        Nil => false,
+        bool b => b,
+        _ => true,
+    };
+}
